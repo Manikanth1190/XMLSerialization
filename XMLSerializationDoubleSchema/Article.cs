7cbf6565f54fa545cd55fd0baa1629481c58119c
Manikanth Kumar <kmanikanth@spglobal.com>
2019-02-04 15:48:55 +0530

    committing

---
+++ XMLSerializationDoubleSchema/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace XMLSerializationDoubleSchema
+{
+    [XmlRoot("ArticleRotElement")]
+    public class Article
+    {
+        
+        [XmlElement()]
+        public string Title {
+            get;set;
+        }
+        public enum choices { Author=0, AuthorFirstname = 1 }
+
+        [XmlChoiceIdentifier("choice")]
+        [XmlElement("Author")]
+        [XmlElement("AuthorFirstname")]
+        public string AuthorName { get; set; }
+
+        public choices AuthorNamechoice { get; set; }
+
+        public string Text { get; set; }
+        public int OrgId { get; set; }
+    }
+}
---
+++ XMLSerializationDoubleSchema/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace XMLSerializationDoubleSchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article obj = new Article() { AuthorName = "Mani", OrgId = 98, Text = "sdfsdfd", Title = "New Article" };
+
+            obj.AuthorNamechoice = Article.choices.AuthorFirstname;
+
+            System.Xml.Serialization.XmlSerializer s = new System.Xml.Serialization.XmlSerializer(typeof(Article));
+            StringBuilder sb = new StringBuilder();
+            
+            XmlWriter xw = XmlWriter.Create(sb);
+
+            s.Serialize(xw, obj);
+            
+            Console.Write(sb.ToString());
+            Console.Read();
+        }
+    }
+}
